2e5aae7f6e8f2f85660b07e978a2ff66a0ed3fea
Parv-gugnani <9765.parv@gmail.com>
2023-08-22 10:41:10 +0530

    11th module completed!

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,31 +1,48 @@
 import { useState } from "react";
+import { GoChevronDown, GoChevronLeft } from "react-icons/go";
 
 function Accordion({ items }) {
-  const [expandedIndex, setExpandedIndex] = useState(0); //changed the extned one like (1) == then changed one decp
-  //
-  const renderedItems = items.map((items, index) => {
+  const [expandedIndex, setExpandedIndex] = useState(0);
+
+  const renderedItems = items.map((item, index) => {
     const isExpanded = index === expandedIndex;
 
+    const icon = (
+      <span className="text-4xl">
+        {isExpanded ? <GoChevronLeft /> : <GoChevronDown />}
+      </span>
+    );
     // long hand approach
     // you will not see thisis thing in professionals projects
     // if we need to upload various handle functions
+
     const handleClick = (nextIndex) => {
-      setExpandedIndex(nextIndex);
+      if (expandedIndex === nextIndex) {
+        setExpandedIndex(-1);
+      } else {
+        setExpandedIndex(nextIndex);
+      }
     };
-    // conditional rendering ->>>>>>>>
+
+    //conditionally rendering
     return (
-      <div>
-        <div onClick={() => handleClick(index)}>{items.label}</div>
-        {isExpanded && <div>{items.content}</div>}
+      <div key={index}>
+        <div
+          className="flex justify-between p-3 bg-gray-50 border-b items-center cursor-pointer"
+          onClick={() => handleClick(index)}
+        >
+          {item.label}
+          {icon}
+        </div>
+        {isExpanded && <div className="border-b p-5">{item.content}</div>}
       </div>
     );
   });
-  return;
-  <div>{renderedItems}</div>;
+
+  return <div className="border-x border-t roundeds">{renderedItems}</div>;
 }
 
 export default Accordion;
-
 /**
  * truthy falsy value
  * hi || there   === truthy value

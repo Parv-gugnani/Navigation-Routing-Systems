0faeec2a15070b72c3bce6a46008c0152a3e22ce
Parv-gugnani <9765.parv@gmail.com>
2023-08-28 10:13:01 +0530

    navigation

--- src/context/navigation.js
+++ src/context/navigation.js
@@ -1,10 +1,34 @@
-import { Children, createContext } from "react";
+import { Children, createContext , useState , useEffect } from "react";
 
 
 const NavigationContext = createContext();
 
-const NavigationProvider(){
-    return <NavigationContext.Provider value={{}}>
-        {Children}
-    </NavigationContext.Provider>;
+const NavigationProvider({ children })
+{
+    const [currentPath ,setCurrentPath] = useState(window.location.pathname); 
+
+    useEffect(() =>{
+        const handler=()=>{
+            setCurrentPath(window.location.pathname);
+
+
+        };
+        window.addEventListener("popstate", handler );
+        
+        return ()=>{
+            window.removeEventListener("popstate", handler );
+        };
+
+    },[]);
+
+    return( 
+    <NavigationContext.Provider value={{}}>
+        {currentPath}
+        {children}
+    </NavigationContext.Provider>
+    );
 }
+
+
+export {NavigationProvider};
+export default NavigationProvider;

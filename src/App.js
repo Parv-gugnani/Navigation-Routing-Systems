fa3b18da470a31f1cd42f90f1c5fe551c12a95d8
Parv-gugnani <9765.parv@gmail.com>
2023-08-28 12:38:31 +0530

    link

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
+import Link from "./components/Link";
 import { useState } from "react";
 import Dropdown from "./components/Dropdown";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Link to="/accordion"></Link>
+      <Link to="/dropdown"></Link>
+    </div>
+  );
 }
 
 export default App;
--- src/context/navigation.js
+++ src/context/navigation.js
@@ -1,34 +1,32 @@
-import { Children, createContext , useState , useEffect } from "react";
-
+import { createContext, useState, useEffect } from "react";
 
 const NavigationContext = createContext();
 
-const NavigationProvider({ children })
-{
-    const [currentPath ,setCurrentPath] = useState(window.location.pathname); 
+function NavigationProvider({ children }) {
+  const [currentPath, setCurrentPath] = useState(window.location.pathname);
 
-    useEffect(() =>{
-        const handler=()=>{
-            setCurrentPath(window.location.pathname);
+  useEffect(() => {
+    const handler = () => {
+      setCurrentPath(window.location.pathname);
+    };
+    window.addEventListener("popstate", handler);
 
+    return () => {
+      window.removeEventListener("popstate", handler);
+    };
+  }, []);
 
-        };
-        window.addEventListener("popstate", handler );
-        
-        return ()=>{
-            window.removeEventListener("popstate", handler );
-        };
+  const navigate = (to) => {
+    window.history.pushState({}, "", to);
+    setCurrentPath(to);
+  };
 
-    },[]);
-
-    return( 
-    <NavigationContext.Provider value={{}}>
-        {currentPath}
-        {children}
+  return (
+    <NavigationContext.Provider value={{ currentPath, navigate }}>
+      {children}
     </NavigationContext.Provider>
-    );
+  );
 }
 
-
-export {NavigationProvider};
-export default NavigationProvider;
+export { NavigationProvider };
+export default NavigationContext;

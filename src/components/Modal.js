0c021197dac5a7ec8c643760b41a74485173e971
Parv-gugnani <9765.parv@gmail.com>
2023-09-02 10:39:03 +0530

    14th Module Completed!

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,14 +1,22 @@
 import React, { Children } from "react";
 import { createPortal } from "react-dom";
+import { useEffect } from "react";
 
 function Modal({ onClose, children, actionBar }) {
+  useEffect(() => {
+    document.body.classList.add("overflow-hidden");
+
+    return () => {
+      document.body.classList.remove("overflow-hidden");
+    };
+  }, []);
   return createPortal(
     <div>
       <div
         onClick={onClose}
-        className="absolute inset-0 bg-gray-300 opacity-80"
+        className="fixed inset-0 bg-gray-300 opacity-80"
       ></div>
-      <div className="absolute inset-40 p-10 bg-white">
+      <div className="fixed inset-40 p-10 bg-white">
         <div className="flex flex-col justify-between h-full">
           {children}
           <div className="flex justify-end">{actionBar}</div>

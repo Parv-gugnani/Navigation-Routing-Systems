0904ef0c882293650052df14c69431bde3d2e520
Parv-gugnani <9765.parv@gmail.com>
2023-09-02 15:06:00 +0530

    maps

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,5 +1,24 @@
-function Table() {
-  return <div>Table</div>;
+function Table({ data, config }) {
+  const renderedHeaders = config.map((column) => {
+    return <th key={column.label}>{column.label}</th>;
+  });
+  const renderedRows = data.map((fruit) => {
+    return (
+      <tr className="border-b" key={fruit.name}>
+        <td className="p-3">{config[0].render(fruit)}</td>
+        <td className="p-3">{config[1].render(fruit)}</td>
+        <td className="p-3">{config[2].render(fruit)}</td>
+      </tr>
+    );
+  });
+  return (
+    <table className="table-auto border-spacing-2">
+      <thead>
+        <tr className="border-b-2">{renderedHeaders}</tr>
+      </thead>
+      <tbody>{renderedRows}</tbody>
+    </table>
+  );
 }
 
 export default Table;
--- src/pages/TablePage.js
+++ src/pages/TablePage.js
@@ -1,9 +1,22 @@
 import Table from "../components/Table";
 
 function TablePage() {
+  const data = [
+    { name: "Orange", color: "bg-orange-500", score: 5 },
+    { name: "lime", color: "bg-red-500", score: 2 },
+    { name: "Apple", color: "bg-yellow-500", score: 3 },
+    { name: "banana", color: "bg-green-500", score: 4 },
+  ];
+
+  const config = [
+    { label: "Name", render: (fruit) => fruit.name },
+    { label: "Color", render: (fruit) => fruit.color },
+    { label: "Score", render: (fruit) => fruit.score },
+  ];
+
   return (
     <div>
-      <Table />
+      <Table data={data} config={config} />
     </div>
   );
 }

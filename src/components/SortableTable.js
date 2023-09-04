922096c1923e6457094aa64b8c3356c3bc7f7467
Parv-gugnani <9765.parv@gmail.com>
2023-09-04 17:03:13 +0530

    sort table

--- src/components/SortableTable.js
+++ src/components/SortableTable.js
@@ -1,7 +1,45 @@
 import Table from "./Table";
+import { useState } from "react";
 
 function SortableTable(props) {
-  return <Table {...props} />;
+  const [sortOrder, setSortOrder] = useState(null);
+  const [sortBy, setSortBy] = useState(null);
+  const { config } = props;
+
+  const handleClick = () => {
+    if(sortOrder === null){
+      setSortOrder('asc');
+      setSortBy(label);
+    }else if(sortOrder === 'asc'{
+      setSortOrder('desc');
+      setSortBy(label);
+    }else if(sortOrder === 'desc'){
+      setSortOrder(null);
+      setSortBy(null);
+    }
+  };
+
+  const updatedConfig = config.map((column) => {
+    if (!column.sortValue) {
+      return column;
+    }
+    return {
+      ...column,
+      header: () => (
+        <th onClick={() => handleClick(column.label)}>
+          {column.label} is Sortable
+        </th>
+      ),
+    };
+  });
+  return <div>
+    {sortOrder} - {sortBy};
+    <Table {...props} config={updatedConfig} />;
+    </div>
 }
 
 export default SortableTable;
+
+
+// asc for starting 
+//desc for endings

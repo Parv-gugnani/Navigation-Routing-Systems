2ba184d9d14ac88810c7dfeee02a3b12956e4d4a
Parv-gugnani <9765.parv@gmail.com>
2023-09-04 08:41:16 +0530

    sortable

---
+++ src/components/SortableTable.js
@@ -0,0 +1,7 @@
+import Table from "./Table";
+
+function SortableTable(props) {
+  return <Table {...props} />;
+}
+
+export default SortableTable;
--- src/pages/TablePage.js
+++ src/pages/TablePage.js
@@ -1,4 +1,5 @@
-import Table from "../components/Table";
+// import Table from "../components/Table";
+import SortableTable from "../components/SortableTable";
 
 function TablePage() {
   const data = [
@@ -9,11 +10,16 @@ function TablePage() {
   ];
 
   const config = [
-    { label: "Name", render: (fruit) => fruit.name },
+    {
+      label: "Name",
+      render: (fruit) => fruit.name,
+      sortValue: (fruit) => fruit.name,
+    },
     { label: "Color", render: (fruit) => fruit.color },
     {
       label: "Score",
       render: (fruit) => fruit.score,
+      sortValue: (fruit) => fruit.score,
     },
   ];
 
@@ -24,7 +30,7 @@ function TablePage() {
   // ** this is square in js
   return (
     <div>
-      <Table data={data} config={config} keyFn={keyFn} />
+      <SortableTable data={data} config={config} keyFn={keyFn} />
     </div>
   );
 }

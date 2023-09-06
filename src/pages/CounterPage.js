4dc17b308d433203a9bd6e3d6c98e16604d6eac1
Parv-gugnani <9765.parv@gmail.com>
2023-09-06 13:08:16 +0530

    useReducer

--- src/pages/CounterPage.js
+++ src/pages/CounterPage.js
@@ -1,14 +1,46 @@
 import Button from "../components/Button";
-import useCounter from "../hooks/use-counter";
+import { useState } from "react";
+import Panel from "../components/Panel";
 
 function CounterPage({ initialCount }) {
-  const { count, increment } = useCounter(initialCount);
+  const [count, setCount] = useState(initialCount);
+  const [valueToAdd, setValueToAdd] = useState(0);
 
+  const increment = () => {
+    setCount(count + 1);
+  };
+  const decrement = () => {
+    setCount(count - 1);
+  };
+
+  const handleChange = () => {
+    const value = event.target.value;
+
+    console.log(value);
+  };
+
+  //
+  //
+  //
   return (
-    <div>
-      <h1>Count is {count}</h1>
-      <Button onClick={increment}>Increment</Button>
-    </div>
+    <Panel className="m-3">
+      <h1 className="text-lg">Count is {count}</h1>
+      <div className="flex flex-row">
+        <Button onClick={increment}>Increment</Button>
+        <Button onClick={decrement}>Decrement</Button>
+      </div>
+
+      <form>
+        <label>Add a lot!</label>
+        <input
+          value={valueToAdd}
+          onChange={handleChange}
+          type="number"
+          className="p-1 m-3 bg-gray-50 border border-gray-300"
+        />
+        <button>Add it!</button>
+      </form>
+    </Panel>
   );
 }
 
